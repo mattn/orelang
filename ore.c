72260be5d8de6b859cab533c2f3d29185d045d9f
mattn <mattn.jp@gmail.com>
2014-04-15 12:43:24 +0900

    ore cfunc

--- ore.c
+++ ore.c
@@ -20,6 +20,8 @@
 "  eof       : /$/ ;                                         \n" \
 "  stmts     : (<let> | <call> | <comment>)* <eof> ;         \n"
 
+void ore_free(void *p);
+
 #define is_a(t, a) (strstr(t->tag, a) != NULL)
 
 #define ORE_TYPE_NIL 0
@@ -27,17 +29,17 @@
 #define ORE_TYPE_STR 2
 #define ORE_TYPE_FNC 3
 
-typedef struct {
+typedef struct _ore_value {
   int t;
   union _v {
     int i;
     char* s;
+    void* f;
   } v;
 } ore_value;
 
 KHASH_MAP_INIT_STR(ident, ore_value)
 
-void ore_free(void *p);
 KLIST_INIT(ident, ore_value, ore_free)
 
 typedef struct {
@@ -45,15 +47,16 @@ typedef struct {
   klist_t(ident) *gc;
 } ore_context;
 
+typedef ore_value (*ore_func_t)(ore_context*, int, ore_value*);
+
+ore_value ore_call(ore_context*, mpc_ast_t*);
+ore_value ore_eval(ore_context*, mpc_ast_t*);
+
 void
 ore_free(void *p) {
   ore_value* v = (ore_value*) p;
   if (v->t == ORE_TYPE_STR)
     free(v->v.s);
-}
-
-ore_value
-ore_call(ore_func ) {
 }
 
 ore_value
@@ -77,6 +80,58 @@ ore_value_str(ore_context* ore, const char* s) {
   strncpy(v.v.s, s + 1, l);
   v.v.s[l] = 0;
   *kl_pushp(ident, ore->gc) = v;
+  return v;
+}
+
+ore_value
+ore_println(ore_context* ore, int num_in, ore_value* args) {
+  int i;
+  for (i = 0; i < num_in; i++) {
+    if (i != 0) printf(", ");
+    switch (args[i].t) {
+      case ORE_TYPE_NIL:
+        printf("nil");
+        break;
+      case ORE_TYPE_NUM:
+        printf("%d", args[i].v.i);
+        break;
+      case ORE_TYPE_STR:
+        printf("%s", args[i].v.s);
+        break;
+      default:
+        printf("<unkonwn>");
+        break;
+    }
+  }
+  printf("\n");
+  return ore_value_nil();
+}
+
+ore_value
+ore_define_cfunc(ore_context* ore, const char* name, ore_func_t f) {
+  int r = 0;
+  khint_t k = kh_put(ident, ore->env, name, &r);
+  ore_value v = { ORE_TYPE_FNC };
+  v.v.f = f;
+  kh_value(ore->env, k) = v;
+  return v;
+}
+
+ore_value
+ore_call(ore_context* ore, mpc_ast_t *t) {
+  khint_t k = kh_get(ident, ore->env, t->children[0]->contents);
+  if (k == kh_end(ore->env)) {
+    fprintf(stderr, "Unknwn function '%s'\n", t->children[0]->contents);
+    return ore_value_nil();
+  }
+  ore_value f = kh_value(ore->env, k);
+  int num_in = t->children_num / 2 - 1, n = 0, i;
+  ore_value* args = (ore_value*) malloc(sizeof(ore_value) * num_in);
+  for (i = 2; i < t->children_num - 2; i += 2) {
+    args[n++] = ore_eval(ore, t->children[i]);
+  }
+  ore_value v = ((ore_func_t)f.v.f) (ore, num_in, args);
+  free(args);
   return v;
 }
 
@@ -125,28 +180,7 @@ ore_eval(ore_context* ore, mpc_ast_t* t) {
     return v;
   }
   if (is_a(t, "call")) {
-    int r = 0;
-    if (!strcmp(t->children[0]->contents, "println")) {
-      for (i = 2; i < t->children_num - 2; i += 2) {
-        if (i != 2) printf(", ");
-        v = ore_eval(ore, t->children[i]);
-        switch (v.t) {
-          case ORE_TYPE_NIL:
-            printf("nil");
-            break;
-          case ORE_TYPE_NUM:
-            printf("%d", v.v.i);
-            break;
-          case ORE_TYPE_STR:
-            printf("%s", v.v.s);
-            break;
-        }
-      }
-      printf("\n");
-    } else {
-      fprintf(stderr, "Unknwn function '%s'\n", t->children[0]->contents);
-    }
-    return ore_value_nil();
+    return ore_call(ore, t);
   }
   if (t->tag[0] == '>') {
     for (i = 0; i < t->children_num; i++) {
@@ -206,6 +240,7 @@ int main(int argc, char **argv) {
   mpc_ast_print(result.output);
 
   ore_context* ore = ore_new();
+  ore_define_cfunc(ore, "println", ore_println);
   ore_eval(ore, result.output);
   mpc_ast_delete(result.output);
   ore_destroy(ore);
